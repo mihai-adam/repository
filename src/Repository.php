92a8994867681c6b0eeb1567c6ea94b468e6bee2
Mihai Adam <mihai.adam@complyadvantage.com>
2016-12-23 16:06:43 +0200

    move repository from laravel repository

--- src/Repository.php
+++ src/Repository.php
@@ -1,17 +1,22 @@
 <?php
+
 namespace Dynamicart\Repository;
+
 /**
  * Base Repository Class
  *
  * Class Repository
- * @package Dynamicart\Repository
+ * @package Dynamicart\LaravelRepository
  */
 abstract class Repository
 {
+
     /**
      * Model class name
      *
      * @return string
      */
     public abstract function getModelClass();
+
+
 }
